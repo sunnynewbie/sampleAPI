4abfa11f78c42db292df3ee8d5d6ea5139d861d5
sunnynewbie <sunny.ptel5497@gmail.com>
2022-11-15 00:48:00 +0530

    init 10

--- api/index.js
+++ api/index.js
@@ -1,14 +1,8 @@
 const express=require('express')
 const cors=require('cors');
-
-const dotenv = require('dotenv');
-dotenv.config();
-
-const userRoute=require('../routes/userRoute')
-const diaryRoute=require('../routes/diaryRoutes')
+const userRoute=require('./routes/userRoute')
+const diaryRoute=require('./routes/diaryRoutes')
 const { urlencoded, json } = require('body-parser');
-
-const http =require('http')
 
 const app=express();
 
@@ -17,18 +11,7 @@ app.subscribe(urlencoded({extends:true}));
 app.use(json());
 app.use(express.static('./public'));
 
-app.use('/users',userRoute)
+app.use('',userRoute)
 app.use('/posts',diaryRoute)
 
-const port=process.env.PORT;
-require('./../database/db/db');
-app.listen(port,()=>{
-    
-console.log('server connected');
-})
-const server= http.createServer(app);
-
-server.listen(port,()=>{
-console.log('server connected');
-});
 module.exports=app;
